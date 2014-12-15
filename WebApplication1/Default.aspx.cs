e4093fe00481bbcf2b821a9b8c54f41f033d39a2
Ivan Popov <unfragable@data.bg>
2014-12-15 16:39:34 +0200

    added CS chapter 2 in bg

--- WebApplication1/Default.aspx.cs
+++ WebApplication1/Default.aspx.cs
@@ -276,6 +276,8 @@ namespace WebApplication1
                     {
                         case 1: getText("CSBul1", 1);
                             break;
+                        case 2: getText("CSBul2", 1);
+                            break;
                     }
                 }
                 else
@@ -284,6 +286,8 @@ namespace WebApplication1
                     {
                         case 1: getText("CSEng1", 1);
                             break;
+                        //case 2: getText("CSEng2", 1);
+                            //break;
                     }
                     chapter1Btn.Text = "Chapter 1. Introduction to Programming";
                     chapter2Btn.Text = "Chapter 2. Primitive Types and Variables";
@@ -449,6 +453,11 @@ namespace WebApplication1
         protected void CSBul1_Click(object sender, EventArgs e)
         {
             setQueryString("p", "1");
+        }
+
+        protected void CSBul2_Click(object sender, EventArgs e)
+        {
+            setQueryString("p", "2");
         }
 
         protected void backBtn_Click(object sender, EventArgs e)
